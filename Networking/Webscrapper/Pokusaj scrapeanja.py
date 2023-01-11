81171bfc6408639561aafb7831aa1c3a7da737ff
skrubitos <119929416+skrubitos@users.noreply.github.com>
2023-01-11 21:02:32 +0100

    scrape njuskalo

--- Networking/Webscrapper/Pokusaj scrapeanja.py
+++ Networking/Webscrapper/Pokusaj scrapeanja.py
@@ -1,5 +1,7 @@
-#ovo je nesto novo
+import urllib.request
+from bs4 import BeautifulSoup
+
+def extract (page):
+    headers= {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
 
 
-x=2
-f=34
