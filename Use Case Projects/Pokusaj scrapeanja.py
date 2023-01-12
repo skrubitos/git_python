8b239444767f7095d85168111ccd12b8180c04bc
skrubitos <119929416+skrubitos@users.noreply.github.com>
2023-01-12 11:33:08 +0100

    spremljen prvi dio

--- Use Case Projects/Pokusaj scrapeanja.py
+++ Use Case Projects/Pokusaj scrapeanja.py
@@ -2,32 +2,49 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 listofjobs= []
-def extract (page):
+def ime_kompanije(url): #ulazimo u link kompanije te kopiramo ime te firme
+    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
+#url ce biti procesuiran kroz funkciju
+    r=requests.get(url,headers)
+    soup= BeautifulSoup(r.content, "html.parser")
+    job_company = soup.find('li', class_='job-company')
+    company_name = job_company.text
+    return (company_name)
+
+def extract (page):     #ulazimo u stranicu moj-posao.net
     headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
     url= f"https://www.moj-posao.net/Pretraga-Poslova/?searchWord=&keyword=&job_title=&job_title_id=&skill=&skill_id=&area=&category=11&page3=&page={page}"
     r=requests.get(url,headers)
     soup= BeautifulSoup(r.content, "html.parser")
     return soup
 
-def transform (soup):
+def transform (soup): #trazimo podatke
     divs=soup.find_all("div", class_ = "job-data")
     for items in divs:
         try:
             x=items.find("a")
             job_position = x.find("span", class_="job-position").text.strip()
-            print("This is the job position:", job_position)
+            job_position = job_position.replace(" (m/ž)", "").replace(" (m/f)", "").replace("(f/m)", "").replace("(w/m/x)","")
+            trans_table = str.maketrans("ČŠĆĐŽčšćđž", "CSCDZcscdz")
+            job_position = job_position.translate(trans_table)
 
+
+            print("Job position:", job_position, end="" )
+            job_link=x.get("href")
+
+            print(    "Company :",ime_kompanije(job_link))
             job_location=x.find("span", class_ ="job-location").text
-            print("Location:",job_location)
+            #print("Location: ",job_location)
 
             job_date=x.find("time", class_= "deadline").text
-            print("This is the deadline:", job_date)
+            #print("This is the deadline:", job_date)
 
-            job_link=x.get("href")
-            print(job_link)
+
+            #print("Link: ", job_link)
 
             listofjobs.append({
                 "Position": job_position,
+                "Company":ime_kompanije(job_link),
                 "Location": job_location,
                 "Deadline": job_date,
                 "Link" : job_link
@@ -39,9 +56,8 @@ for x in range (1,50):
     goveda=extract(x)
     transform(goveda)
 
-
-print("This is list of jobs: \n",listofjobs)
+print(listofjobs)
 
 #creating csv file
-#jobs_df = pd.DataFrame(listofjobs)
-#jobs_df.to_csv('jobss.csv', index=False)
+jobs_df = pd.DataFrame(listofjobs)
+jobs_df.to_csv('jobss.csv', index=False)
