9e9dfc36eef8082ce5d033fd1a2fd4a3eb7d3391
skrubitos <119929416+skrubitos@users.noreply.github.com>
2023-01-30 12:04:48 +0100

    izbrisali zadnji dio

--- Inflacija/geo_json.py
+++ Inflacija/geo_json.py
@@ -13,21 +13,3 @@ spajanje= urllib.request.urlopen(skupni_url)
 citanje=spajanje.read().decode()
 
 
-print(len(citanje))
-try:
-    js = json.loads(citanje)
-except:
-    js = None
-
-if not js or 'status' not in js or js['status'] != 'OK':
-    print('==== Failure To Retrieve ====')
-    print(citanje)
-
-
-print(json.dumps(js, indent=4))
-
-lat = js['results'][0]['geometry']['location']['lat']
-lng = js['results'][0]['geometry']['location']['lng']
-print('lat', lat, 'lng', lng)
-location = js['results'][0]['formatted_address']
-print(location)
