d8f844cfe808c01c7b2b12748b78b268411472b7
skrubitos <119929416+skrubitos@users.noreply.github.com>
2023-01-31 11:27:39 +0100

    gotov proiyvod

--- Inflacija/geo_json.py
+++ Inflacija/geo_json.py
@@ -1,15 +1,33 @@
 import urllib.request, urllib.parse
 import json
+api_key = 42
+url= 'http://py4e-data.dr-chuck.net/json?'
+#adresa = input("what is your adress? ")
+while True:
+    adresa=input("Upisite svoju ulicu: ")
+    ivanko=dict()
+    ivanko["address"]=adresa
+    ivanko['key']=api_key
+    skupni_url= url + urllib.parse.urlencode(ivanko)
 
-url= "https://py4e-data.dr-chuck.net/json?"
-ivanko=dict()
-adresa = input("what is your adress? ")
-ivanko["adress"]=adresa
-skupni_url= url + urllib.parse.urlencode(ivanko)
+    print(f"Retrieving url: {skupni_url} \n \n")
 
-print(f"Retrieving url: {skupni_url}")
+    spajanje= urllib.request.urlopen(skupni_url)
+    citanje=spajanje.read().decode()
 
-spajanje= urllib.request.urlopen(skupni_url)
-citanje=spajanje.read().decode()
+    js=json.loads(citanje)
+    #print(json.dumps(js,indent=4))
+    
+    try:
+        grad=js['results'][0]['address_components'][2]['long_name']
+        opcina=js['results'][0]['address_components'][3]['long_name']
+        zupanija=js['results'][0]['address_components'][4]['long_name']
+        drzava=js['results'][0]['address_components'][5]['long_name']
 
+        lat=js['results'][0]["geometry"]['location']['lat']
+        lng=js['results'][0]["geometry"]['location']['lng']
 
+        print(f'Ovo su vasi podatci \n Drzava: {drzava} \n Zupanija:{zupanija} \n Opcina: {opcina} \n Grad: {grad} \n Lat: {lat} \n Lng: {lng}')
+        break
+    except :
+        print("Nesto je poslo po krivu, ili ne mozemo naci adresu ili niste upisali dobre podatke \n")
